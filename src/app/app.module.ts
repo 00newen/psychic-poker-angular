ed506a59d4a36437a15a1645a3303ccb7a90af30
Newen Fara <>
2018-09-20 00:45:13 +0200

    adding missing code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { PsychicHandComponent } from './psychic-hand/psychic-hand.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    PsychicHandComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
